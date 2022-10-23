dbd77a238f7d2a9ef9042e96c3764a56f20cb682
Magnus Watn <magnus.watn@softwarelab.no>
2022-10-23 14:54:44 +0200

    Update CT logs update process

--- updatectlogs.py
+++ updatectlogs.py
@@ -9,17 +9,19 @@ import hashlib
 from base64 import b64decode, b64encode
 from operator import itemgetter
 
-import requests
+import httpx
 
 from certpeek import KNOWN_LOGS
 
 
-def main():
-    resp = requests.get("https://www.gstatic.com/ct/log_list/log_list.json")
+def main() -> None:
+    resp = httpx.get("https://www.gstatic.com/ct/log_list/v3/log_list.json")
     resp.raise_for_status()
     js = resp.json()
 
-    for log in js.get("logs"):
+    logs = [log for operator in js["operators"] for log in operator["logs"]]
+
+    for log in logs:
         sha256 = hashlib.sha256()
         log_desc = log.get("description")
         log_key = b64decode(log.get("key"))
