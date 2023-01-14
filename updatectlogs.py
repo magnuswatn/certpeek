713998814a3677a88891884970530a1870b9273f
Magnus Watn <magnus@watn.no>
2023-01-14 14:38:51 +0100

    Make updatectlogs update the source itself

--- updatectlogs.py
+++ updatectlogs.py
@@ -3,15 +3,20 @@
 Update the list of known CT logs
 
 Run it like this:
-> ./updatectlogs.py | black -
+> ./updatectlogs.py
 """
 import hashlib
+import re
+import subprocess
 from base64 import b64decode, b64encode
 from operator import itemgetter
+from pathlib import Path
 
 import httpx
 
 from certpeek import KNOWN_LOGS
+
+CERTPEEK_SOURCE_FILE = Path("certpeek.py")
 
 
 def main() -> None:
@@ -29,8 +34,19 @@ def main() -> None:
         log_id = b64encode(sha256.digest()).decode()
         KNOWN_LOGS.update({log_id: log_desc})
 
-    print("KNOWN_LOGS=", end="")
-    print({id: desc for id, desc in sorted(KNOWN_LOGS.items(), key=itemgetter(1))})
+    unformatted_new_known_logs_list = "KNOWN_LOGS="
+    unformatted_new_known_logs_list += str(
+        {id: desc for id, desc in sorted(KNOWN_LOGS.items(), key=itemgetter(1))}
+    )
+    new_known_logs_list = subprocess.check_output(
+        ["black", "--code", unformatted_new_known_logs_list]
+    ).decode()
+
+    certpeek_source = CERTPEEK_SOURCE_FILE.read_text()
+    new_certpeek_source = re.sub(
+        r"KNOWN_LOGS = {[^}]+}\n", new_known_logs_list, certpeek_source
+    )
+    CERTPEEK_SOURCE_FILE.write_text(new_certpeek_source)
 
 
 if __name__ == "__main__":
